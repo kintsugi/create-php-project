3248a24b6f201d3f56fa3b0a5cd7efebc5699c1b
Kyle Salter <kyle@kylesalter.com>
2018-10-20 16:44:37 -0700

    its done!

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM php:7.1.16-apache
-
-MAINTAINER Paul Redmond
-
-COPY .docker/vhost.conf /etc/apache2/sites-available/000-default.conf
-
-RUN chown -R www-data:www-data /srv/app \
-  && a2enmod rewrite
--- new-php-project.sh
+++ new-php-project.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cr=${cr%.}
-read -r -p "This will create a php project folder for you using Docker. Continue? [y\N] $cr"
+read -r -p "This will create a php project folder for you using Docker. Continue? [y\\N] $cr"
 read -r -p "Enter the name for your php project: $cr" name 
 read -r -p "Enter the directory for your php project: $cr" dir
-composer create-project $dir
+mkdir $dir
 cd $dir || exit;
+echo 'Initializing git'
+git init
+cp $script_dir/new-php-project.gitignore .gitignore
+echo 'Initializing composer'
+composer init
+echo 'Initializing docker'
 mkdir docker/
 cp $script_dir/docker/Dockerfile docker/Dockerfile 
 cp $script_dir/docker/vhost.conf docker/vhost.conf
-cp $script_dir/docker/docker-compose.yml .
+cp $script_dir/docker-compose.yml .
+cp -r $script_dir/src .
 sed -i bak -e "s|PROJECT_NAME|${name}|g" docker-compose.yml
-docker build \
-  --file docker/Dockerfile \
-  -t $name-php-docker .
+docker-compose build
+echo "New project created! Run the following command to run and then view the phpinfo at http://localhost:8080/index.php"
+echo "cd $dir && docker-compose up"
